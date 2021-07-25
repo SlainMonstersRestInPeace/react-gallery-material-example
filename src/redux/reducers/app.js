e2591903e2b7fa50cf64d6f131f7dcd2ab747a7c
sergey stenyanskiy <qweartekyl@gmail.com>
2021-07-25 20:56:16 +0300

    initial

---
+++ src/redux/reducers/app.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+
+}
+
+const appSlice = createSlice({
+  name: 'app',
+  initialState,
+  reducers: {
+
+  }
+});
+
+const {
+
+} = appSlice.actions;
+
+export default appSlice.reducer
