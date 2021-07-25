8a7a3c8fa12646d9e07935530ab31f14fac50f74
sergey stenyanskiy <qweartekyl@gmail.com>
2021-07-25 21:16:28 +0300

    added basic routing and stuff

--- src/views/MainView.js
+++ src/views/MainView.js
@@ -1,7 +1,25 @@
 import React from 'react'
 
+import {
+  Switch, Route
+} from 'react-router-dom'
+
+import Header from '../components/Header'
+
+import Gallery from './Gallery'
+import About from './About'
+import ImageInfo from './ImageInfo'
+
 export default () => {
   return (
-    <>MainView Placeholder</>
+    <>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Gallery} />
+        <Route path="/gallery" component={Gallery} />
+        <Route path="/about" component={About} />
+        <Route path="/image/:id" component={ImageInfo} />
+      </Switch>
+    </>
   );
 }
